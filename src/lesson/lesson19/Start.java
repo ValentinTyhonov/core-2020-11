518e77d80ecc916e7d9fa15248c3be087cabe98d
valentin-tyhonov <valentin.tyhonov@ancestry.com>
2021-02-18 13:53:24 +0200

    Executors

--- src/lesson/lesson19/Start.java
+++ src/lesson/lesson19/Start.java
@@ -2,6 +2,8 @@ package lesson.lesson19;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -19,13 +21,18 @@ public class Start
 
         Thread thread = new Thread(new PeopleQueueRun(women, 3423));
 
-        menQueue.start();
-        menQueue2.setDaemon(true);
-        menQueue2.start();
-        womenQueue.start();
-        thread.start();
+//        menQueue.start();
+//        menQueue2.setDaemon(true);
+//        menQueue2.start();
+//        womenQueue.start();
+//        thread.start();
 
 
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        executorService.execute(new PeopleQueueRun(women, 3423));
+        executorService.execute(new PeopleQueueRun(women, 3200));
+        executorService.execute(new PeopleQueueRun(women, 3045));
+        executorService.execute(new PeopleQueueRun(women, 5644));
 
 
 
